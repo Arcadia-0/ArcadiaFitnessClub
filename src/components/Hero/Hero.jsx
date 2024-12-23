e8e7adef5e2c8e8f2dd311f6a136b16d7f7097f2
Arcadia-0 <business.ydeniz@gmail.com>
2024-12-23 12:59:10 +0300

    hero section added

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,36 @@
+import heroImage from "../../assets/heroImage.png";
+
+const Hero = () => {
+  return (
+    <div className="container mx-auto px-4 py-16">
+      <div className="flex flex-col md:flex-row items-center md:justify-between gap-4">
+       
+        <div className="text-white md:w-1/2 text-center md:text-left">
+          <h1 className="text-2xl md:text-4xl font-semibold mb-4">
+           <span className="text-primary">Arcadia Fitness Club</span> has been serving in Helsinki since 2004.
+          </h1>
+          <p className="text-base md:text-lg mb-6 leading-relaxed">
+            Our 8,000 m² facility, with its newly renovated architecture focused
+            entirely on healthy living and sports, continues to operate as one of
+            Finlands largest sports and wellness centers. We understand that physical
+            activity alone is not enough for a healthy lifestyle, and factors such as
+            nutrition, psychology, physical therapy, and enjoyable living are also
+            important. With personalized programs tailored to various goals such as weight
+            loss, weight gain, spot fat reduction, staying fit, and correcting posture
+            problems, we help you achieve your objectives.
+          </p>
+        </div>
+       
+        <div className="w-full md:w-1/2 flex justify-center md:justify-end">
+          <img
+            src={heroImage}
+            alt="Hero Image"
+            className="w-full h-auto max-w-none md:max-w-full rounded-lg shadow-lg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
