b1c1d3371c7a64cd5202f8bedf9d5ee1a1bfd83b
Arcadia-0 <business.ydeniz@gmail.com>
2024-12-30 14:08:58 +0300

    changed react-slick to swiper. Still some issues

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,64 +1,67 @@
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';  
+import { Navigation } from 'swiper/modules'; 
 import image_1 from "../../assets/gallery/image-1.png";
 import image_2 from "../../assets/gallery/image-2.png";
 import image_3 from "../../assets/gallery/image-3.png";
 import image_4 from "../../assets/gallery/image-4.png";
 
-// Slick slider settings
-const settings = {
-  dots: false, 
-  infinite: true, 
-  speed: 500,  
-  slidesToShow: 3, 
-  slidesToScroll: 1,  
-  autoplay: false,  
-  autoplaySpeed: 2000,  
-  responsive: [
-    {
-      breakpoint: 1024, // Tablets and below
-      settings: {
-        slidesToShow: 1,
-      },
-    },
-    {
-      breakpoint: 768, // Mobile devices
-      settings: {
-        slidesToShow: 1,
-        arrows: false,  // Hide arrows on smaller screens
-      },
-    },
-  ],
-};
-
 const Gallery = () => {
   return (
     <div className="container mx-auto py-10 px-4">
-      <Slider {...settings}>
-        <div className="relative">
-          <img src={image_1} alt="image 1" className="w-full rounded-lg shadow-lg" />
-        </div>
-        <div className="relative">
-          <img src={image_2} alt="image 2" className="w-full rounded-lg shadow-lg" />
-        </div>
-        <div className="relative">
-          <img src={image_3} alt="image 3" className="w-full rounded-lg shadow-lg" />
-        </div>
-        <div className="relative">
-          <img src={image_4} alt="image 4" className="w-full rounded-lg shadow-lg" />
-        </div>
-        <div className="relative">
-          <img src={image_1} alt="image 1" className="w-full rounded-lg shadow-lg" />
-        </div>
-        <div className="relative">
-          <img src={image_2} alt="image 2" className="w-full rounded-lg shadow-lg" />
-        </div>
-        <div className="relative">
-          <img src={image_3} alt="image 3" className="w-full rounded-lg shadow-lg" />
-        </div>
-        <div className="relative">
-          <img src={image_4} alt="image 4" className="w-full rounded-lg shadow-lg" />
-        </div>
-      </Slider>
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={4} 
+        loop={true} 
+        navigation={true} 
+        modules={[Navigation]} 
+        breakpoints={{
+          1024: {
+            slidesPerView: 4, // 1024px and above shows 4 slides
+            spaceBetween: 30, // Adjust space between slides for larger screens
+          },
+          768: {
+            slidesPerView: 3, 
+            spaceBetween: 20, 
+          },
+          440: {
+            slidesPerView: 2, 
+            spaceBetween: 15, 
+          },
+          0: {
+            slidesPerView: 1, 
+            spaceBetween: 10, 
+          },
+         
+        }}
+        
+      >
+        {/* SwiperSlide elements */}
+        <SwiperSlide>
+          <img src={image_1} alt="Image 1" className="w-full rounded-lg shadow-lg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image_2} alt="Image 2" className="w-full rounded-lg shadow-lg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image_3} alt="Image 3" className="w-full rounded-lg shadow-lg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image_4} alt="Image 4" className="w-full rounded-lg shadow-lg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image_1} alt="Image 1" className="w-full rounded-lg shadow-lg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image_2} alt="Image 2" className="w-full rounded-lg shadow-lg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image_3} alt="Image 3" className="w-full rounded-lg shadow-lg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image_4} alt="Image 4" className="w-full rounded-lg shadow-lg" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
